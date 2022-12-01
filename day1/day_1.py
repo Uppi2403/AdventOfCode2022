241d3429dfaf25e1c8a5061ef8353f42f0348b97
Pascal Uphaus <uppi2403@gmail.com>
2022-12-01 23:34:29 +0100

    added part two of the challenge

--- day1/day_1.py
+++ day1/day_1.py
@@ -1,16 +1,22 @@
-with open('puzzle_input', 'r') as f: # encoding="utf-8"
+with open('puzzle_input', 'r') as f:
     lines = f.readlines()
 
     maxCalories = 0
     actualCalories = 0
+    carryingPerElf = []
     
     for line in lines:
         if line.strip():
-            print(int(line))
-            actualCalories += int(line) 
+            actualCalories += int(line)
         else: # Counter is reset because new elf and possible highscore is saved
+            
             if(actualCalories > maxCalories):
                 maxCalories = actualCalories
+                
+            carryingPerElf.append(actualCalories)
             actualCalories = 0
-            
+
+carryingPerElf.sort()
+sumMostCarrying = sum(carryingPerElf[-3:])
+print(f"sumMostCarrying: {sumMostCarrying}")
 print(f"maxCalories: {maxCalories}")
